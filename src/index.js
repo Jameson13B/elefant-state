f4b1709980863c5630d6e484c9b66034713a8da7
Jameson.Brown <jameson.brown@mx.com>
2020-06-30 18:31:58 -0600

    Fixed props error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useReducer } from 'react'
 
 export const ElefantContext = React.createContext(null)
 
-export const ElefantProvider = () => {
+export const ElefantProvider = ({ reducer, initialState, children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   return <ElefantContext.Provider value={[state, dispatch]}>{children}</ElefantContext.Provider>
